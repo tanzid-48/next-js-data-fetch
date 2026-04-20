80b9b081399c05b537e2dbedc86f6a87b36ec6db
tanzid-48 <mdtanzid.525@gmail.com>
2026-04-20 23:14:43 +0600

    Add Navbar

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsPage = () => {
+    return (
+        <div>
+            <h2>post Are coming Soon </h2>
+        </div>
+    );
+};
+
+export default PostsPage;
